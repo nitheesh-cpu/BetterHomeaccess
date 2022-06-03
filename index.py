ede374b43c85ad6210c33fed3b95f7ee54c47281
nitheeshcpu <nitishkoda@outlook.com>
2022-06-03 11:17:52 -0500

    asd

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 from flask import Flask
 
+app = Flask(__name__)
 
 def main():
-    app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hdsfgssdfgdfg'
 
     from views import views
     from auth import auth
-
+    
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
