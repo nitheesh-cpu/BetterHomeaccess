91b5a2fc0127f8628e91e2c54cefb31fd6cee229
nitheeshcpu <nitishkoda@outlook.com>
2022-06-14 10:57:57 -0500

    added session disconnect

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from website import create_app
+from flask_socketio import SocketIO, emit
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@socketio.on('disconnect')
+def disconnect_user():
+    session.pop('username', None)
+    session.pop('password', None)
+    session.pop('login_data', None)
